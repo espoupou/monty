d717d24440153f3f060934e669f0f55194293599
espoupou <espoirativon@gmail.com>
2023-06-22 03:49:58 -0700

    monty.c

--- monty.c
+++ monty.c
@@ -9,6 +9,16 @@
 
 int main(int ac, char **av)
 {
+	char *error;
+
+	if (ac != 2)
+	{
+		error = error_usage();
+		write(STDERR_FILENO, error, _strlen(error));
+		free(error);
+		exit(EXIT_FAILURE);
+	}
+
 	_init_check(ac);
 	_init_stream(av);
 
