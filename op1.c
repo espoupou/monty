4a18f091d6e586ce8a88b2a6eb3f6f3a5dfe49df
espoupou <espoirativon@gmail.com>
2023-06-22 06:24:40 -0700

    implement sub

--- op1.c
+++ op1.c
@@ -32,3 +32,24 @@ void _nop(stack_t **stack, unsigned int line_number)
 	UNUSED(stack);
 	UNUSED(line_number);
 }
+
+/**
+ * sub - sub the top two elements of the stack
+ * @stack: the stack adress
+ * @line_number: the line number
+ * Return: nothing
+ */
+
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	if (datas.len < 2)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		_free_datas();
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n = (*stack)->next->n - (*stack)->n;
+	_pop(stack, line_number);
+}
+
