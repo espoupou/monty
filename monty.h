876b5b8d6e43b8ff01086af2281a7f5cacb965b6
espoupou <espoirativon@gmail.com>
2023-06-22 02:06:34 -0700

    add pint

--- monty.h
+++ monty.h
@@ -107,6 +107,7 @@ void _free_datas();
 void (*get_op_function(char *opc))(stack_t **stack, unsigned int line_number);
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
 
 /* utils.c */
 stack_t *add_dnodeint(stack_t **head, const int n);
